0836301f497e83bbd7fb3d3f4a92730ef0604438
Alex Proutorov <oprouto1@us.ibm.com>
2019-10-18 15:59:24 +0300

    End of section 3

--- DatingApp.API/DTOs/NewUserDto.cs
+++ DatingApp.API/DTOs/NewUserDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.DTOs
 {
     public class NewUserDto
-    {        
+    {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(16, MinimumLength = 4, ErrorMessage = "You must specify password of lenght between 4 and 8 characters")]
         public string Password { get; set; }
     }
 }

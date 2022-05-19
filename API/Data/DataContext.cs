6339d61125d2ff34482021949304c5e0bf36d9f5
Alex Proutorov <alexmail404@gmail.com>
2022-05-20 00:20:33 +0300

    End of Section 14

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,5 +10,25 @@ namespace API.Data
         }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+                .HasKey(key => new { key.SourceUserId, key.LikedUserId });
+
+            builder.Entity<UserLike>()
+                .HasOne(likingUser => likingUser.SourceUser)
+                .WithMany(likesGiven => likesGiven.LikedUsers)
+                .HasForeignKey(sourceUser => sourceUser.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade); // TODO: Use NoAction for SQL Server. 
+                                                   // Otherwise migration will result in error!  
+            builder.Entity<UserLike>()
+                .HasOne(likedUser => likedUser.LikedUser)
+                .WithMany(likesReceived => likesReceived.LikedByUsers)
+                .HasForeignKey(sourceUser => sourceUser.LikedUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
